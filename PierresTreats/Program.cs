4dcbcd3c165e69e1e722541915ee82b9e122aa55
RemyXVX <remyfranciscoflores@gmail.com>
2023-02-04 23:31:13 -0800

    Updated program to better reflect the project at hand

--- PierresTreats/Program.cs
+++ PierresTreats/Program.cs
@@ -2,32 +2,28 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Bakery.Models;
+using PierresTreats.Models;
 
 
-namespace Bakery
+namespace PierresTreats
 {
   class Program
   {
     static void Main(string[] args)
     {
-
       WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
       builder.Services.AddControllersWithViews();
-
-      builder.Services.AddDbContext<BakeryContext>(
-                        dbContextOptions => dbContextOptions
-                          .UseMySql(
-                            builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
-                          )
-                        )
-                      );
+      builder.Services.AddDbContext<PierresTreatsContext>(
+        dbContextOptions => dbContextOptions
+          .UseMySql(
+            builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+            )
+          )
+        );
 
       builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-                      .AddEntityFrameworkStores<BakeryContext>()
-                      .AddDefaultTokenProviders();
-
+        .AddEntityFrameworkStores<PierresTreatsContext>()
+        .AddDefaultTokenProviders();
       builder.Services.Configure<IdentityOptions>(options => 
       {
         options.Password.RequireDigit = false;
@@ -39,20 +35,15 @@ namespace Bakery
       });
 
       WebApplication app = builder.Build();
-
       // app.UseDeveloperExceptionPage();
       app.UseHttpsRedirection();
       app.UseStaticFiles();
-
       app.UseRouting();
-
       app.UseAuthentication();
       app.UseAuthorization();
-
       app.MapControllerRoute(
-          name: "default",
-          pattern: "{controller=Home}/{action=Index}/{id?}");
-
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
       app.Run();
     }
   }
