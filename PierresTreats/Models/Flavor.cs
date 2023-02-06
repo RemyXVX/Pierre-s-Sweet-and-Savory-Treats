c87d5dd0bca2ffe29c7f88353bc14641d87c822b
RemyXVX <remyfranciscoflores@gmail.com>
2023-02-06 14:00:52 -0800

    Updated to add a description of Flavor

--- PierresTreats/Models/Flavor.cs
+++ PierresTreats/Models/Flavor.cs
@@ -8,6 +8,7 @@ namespace PierresTreats.Models
     public int FlavorId { get; set; }
     [Required(ErrorMessage = "Please fill me with something dawg..")]
     public string FlavorType { get; set; }
+    public string FlavorDescription { get; set; }
     public List<FlavorTreat> JoinEntities { get; }
     public ApplicationUser User { get; set; }
   }
