d00e2982130e10e15b8ca3ab416babd4e6a68964
RemyXVX <remyfranciscoflores@gmail.com>
2023-02-06 14:01:24 -0800

    Updated because I added on to the Flavor

---
+++ PierresTreats/Migrations/20230206211327_FlavorDescription.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PierresTreats.Migrations
+{
+    public partial class FlavorDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FlavorDescription",
+                table: "Flavors",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FlavorDescription",
+                table: "Flavors");
+        }
+    }
+}
