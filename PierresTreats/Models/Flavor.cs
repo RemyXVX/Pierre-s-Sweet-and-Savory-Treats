5c93f489c3754300cb5603905d47ae3d93e77bc2
RemyXVX <remyfranciscoflores@gmail.com>
2023-02-06 15:33:48 -0800

    Add img to fail login and styling

--- PierresTreats/Models/Flavor.cs
+++ PierresTreats/Models/Flavor.cs
@@ -6,7 +6,7 @@ namespace PierresTreats.Models
   public class Flavor
   {
     public int FlavorId { get; set; }
-    [Required(ErrorMessage = "Please fill me with something dawg..")]
+    // [Required(ErrorMessage = "Please fill me with something dawg..")]
     public string FlavorType { get; set; }
     public string FlavorDescription { get; set; }
     public List<FlavorTreat> JoinEntities { get; }
